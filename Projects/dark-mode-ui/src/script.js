a52d86f36c2dd78e1f9f66ef3860d4fafb45b7e1
Sami Munir <samimunir@Samis-MBP.fios-router.home>
2024-05-12 03:01:54 -0400

    dark-mode-ui functionality working & complete

--- Projects/dark-mode-ui/src/script.js
+++ Projects/dark-mode-ui/src/script.js
@@ -1,10 +1,15 @@
 const body = document.body;
 const control_div = document.getElementById('control-div');
 const slider_div = document.getElementById('slider-div');
+const main_div = document.getElementById('main-div');
 control_div.addEventListener('click', () => {
     if (slider_div.style.float == 'left') {
         slider_div.style.float = 'right';
+        body.style.backgroundColor = '#000000';
+        main_div.style.color = '#0099ff';
     } else {
         slider_div.style.float = 'left';
+        body.style.backgroundColor = '#ffffff';
+        main_div.style.color = '#000000';
     }
 });
