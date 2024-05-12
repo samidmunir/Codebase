33e0b6daeaf2f757c407ee9d0416084cc14e7f17
Sami Munir <samimunir@Samis-MBP.fios-router.home>
2024-05-12 04:30:15 -0400

    implementing function print_static_array_stats()

--- Data Structures & Algorithms/static-array-java/static-array/src/StaticArray.java
+++ Data Structures & Algorithms/static-array-java/static-array/src/StaticArray.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 public class StaticArray {
     private final int SIZE_OF_INT = 4;
     private int[] array;
@@ -15,5 +18,14 @@ public class StaticArray {
             number_of_elements = 0;
             System.out.println("\nSuccessfully initialized static_array[] with capacity " + this.capacity + ".");
         }
+        print_static_array_stats();
+    }
+
+    private void print_static_array_stats() {
+        System.out.println("static_array[]: " + Arrays.toString(array));
+        System.out.println("\tpointer: " + pointer);
+        System.out.println("\tnumber_of_elements: " + number_of_elements);
+        System.out.println("\tused mem: " + (number_of_elements * SIZE_OF_INT) + " bytes");
+        System.out.println("\tavail. mem: " + ((capacity * SIZE_OF_INT) - (number_of_elements * SIZE_OF_INT)) + " bytes");
     }
 }
