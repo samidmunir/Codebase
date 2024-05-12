31fca475cfe71dd5b66e3cac01dac1ae943427e8
Sami Munir <samimunir@Samis-MBP.fios-router.home>
2024-05-11 23:31:24 -0400

    adding filler main section w/ animation & footer

--- Projects/dark-mode-ui/src/script.js
+++ Projects/dark-mode-ui/src/script.js
@@ -1,1 +1,10 @@
+const body = document.body;
+const control_div = document.getElementById('control-div');
 const slider_div = document.getElementById('slider-div');
+control_div.addEventListener('click', () => {
+    if (slider_div.style.float == 'left') {
+        slider_div.style.float = 'right';
+    } else {
+        slider_div.style.float = 'left';
+    }
+});
