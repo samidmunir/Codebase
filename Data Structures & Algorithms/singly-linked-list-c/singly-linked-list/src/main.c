b558e72b93888117cf351024855775d1d388571b
Sami Munir <sami.munir2001@gmail.com>
2024-05-14 16:25:31 -0400

    creating data structure: singly-linked-list (C implementation)

---
+++ Data Structures & Algorithms/singly-linked-list-c/singly-linked-list/src/main.c
@@ -0,0 +1,17 @@
+/*
+    Sami Munir
+    Data Structures & Algorithms
+    Singly Linked-list | C implementation
+    - main.c
+    - singly_linked_list.h
+    - singly_linked_list.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    printf("\nsingly linked-list | implementation in C\n");
+    
+    return EXIT_SUCCESS;
+}
