6c9c3418062dfd27dfae9fe97ad6462b45a4a6ce
Sami Munir <sami.munir2001@gmail.com>
2024-05-14 17:20:16 -0400

    implementing function print_singly_linked_list()

--- Data Structures & Algorithms/singly-linked-list-c/singly-linked-list/src/singly-linked-list.c
+++ Data Structures & Algorithms/singly-linked-list-c/singly-linked-list/src/singly-linked-list.c
@@ -3,6 +3,25 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+void print_singly_linked_list(struct singly_linked_list_node *head) {
+    printf("singly-linked-list: ");
+    if (head == NULL) {
+        printf("HEAD -> NULL\n");
+        return;
+    } else {
+        struct singly_linked_list_node *current_node = head;
+        printf("HEAD -> ");
+        while (current_node != NULL) {
+            printf("%d -> ", current_node -> data);
+            current_node = current_node -> next;
+        }
+        printf("NULL\n");
+        return;
+    }
+}
+
+void print_singly_linked_list_struct(struct singly_linked_list *linked_list) {}
+
 struct singly_linked_list * initialize_singly_linked_list() {
     printf("\ninitialize_singly_linked_list() called -->\n");
     struct singly_linked_list_node *head = (struct singly_linked_list_node *) malloc(sizeof(struct singly_linked_list_node));
