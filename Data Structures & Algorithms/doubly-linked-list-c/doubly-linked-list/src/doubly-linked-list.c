b1f361a27a1c38d886c9a4e833d02bc421c5929d
Sami Munir <sami.munir2001@gmail.com>
2024-05-22 08:55:07 -0400

    implementing & testing function initialize_doubly_linked_list()

--- Data Structures & Algorithms/doubly-linked-list-c/doubly-linked-list/src/doubly-linked-list.c
+++ Data Structures & Algorithms/doubly-linked-list-c/doubly-linked-list/src/doubly-linked-list.c
@@ -14,13 +14,13 @@ bool is_empty(struct doubly_linked_list *linked_list) {
 void print_doubly_linked_list(struct doubly_linked_list *linked_list) {
     printf("doubly-linked-list: ");
     if (is_empty(linked_list)) {
-        printf("HEAD -> TAIL\n");
+        printf("HEAD <-> TAIL\n");
         return;
     } else {
         struct doubly_linked_list_node *current_node = linked_list -> head;
-        printf("HEAD -> ");
+        printf("HEAD <-> ");
         while (current_node != NULL) {
-            printf("%d -> ", current_node -> data);
+            printf("%d <-> ", current_node -> data);
             current_node = current_node -> next;
         }
         printf("TAIL\n");
@@ -32,4 +32,33 @@ void print_doubly_linked_list_struct(struct doubly_linked_list *linked_list) {
     printf("\tpointer: %d\n", linked_list -> pointer);
     printf("\tnumber_of_elements: %d\n", linked_list -> number_of_elements);
     printf("\tused memory: %lu bytes\n", (linked_list -> number_of_elements * sizeof(struct doubly_linked_list_node)));
+}
+
+struct doubly_linked_list * initialize_doubly_linked_list() {
+    printf("\ninitialize_doubly_linked_list() called -->\n");
+    struct doubly_linked_list_node *head = (struct doubly_linked_list_node *) malloc(sizeof(struct doubly_linked_list_node));
+    if (head == NULL) {
+        printf("--<ERROR>-- memory allocation failure for head node.\n");
+        return NULL;
+    }
+    struct doubly_linked_list_node *tail = (struct doubly_linked_list_node *) malloc(sizeof(struct doubly_linked_list_node));
+    if (tail == NULL) {
+        printf("--<ERROR>-- memory allocation failure for tail node.\n");
+        return NULL;
+    }
+    struct doubly_linked_list *doubly_linked_list_struct = (struct doubly_linked_list *) malloc(sizeof(struct doubly_linked_list));
+    if (doubly_linked_list_struct == NULL) {
+        printf("--<ERROR>-- memory allocation failure for doubly linked-list struct.\n");
+        return NULL;
+    }
+    doubly_linked_list_struct -> head = head;
+    doubly_linked_list_struct -> tail = tail;
+    doubly_linked_list_struct -> pointer = -1;
+    doubly_linked_list_struct -> number_of_elements = 0;
+
+    printf("\tdoubly linked-list successfully initialized.\n");
+    
+    print_doubly_linked_list_struct(doubly_linked_list_struct);
+
+    return doubly_linked_list_struct;
 }
--- Data Structures & Algorithms/doubly-linked-list-c/doubly-linked-list/src/main.c
+++ Data Structures & Algorithms/doubly-linked-list-c/doubly-linked-list/src/main.c
@@ -3,14 +3,22 @@
     Data Structures & Algorithms
     Singly Linked-list | C implementation
     - main.c
+    - doubly_linked_list.h
+    - doubly_linked_list.c
 */
 
+#include "doubly-linked-list.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
     printf("\ndoubly linked-list | implementation in C\n");
     printf("----------------------------------------\n");
+
+    /*
+        Testing function initialize_doubly_linked_list()
+    */
+    struct doubly_linked_list *linked_list  = initialize_doubly_linked_list();
 
     return EXIT_SUCCESS;
 }
