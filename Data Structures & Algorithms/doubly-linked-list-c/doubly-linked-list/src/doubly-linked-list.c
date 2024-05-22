bae0ff93a3b07da0fd1af25a0016c44fb3be9502
Sami Munir <sami.munir2001@gmail.com>
2024-05-22 08:45:38 -0400

    implementing function print_doubly_linked_list_struct()

--- Data Structures & Algorithms/doubly-linked-list-c/doubly-linked-list/src/doubly-linked-list.c
+++ Data Structures & Algorithms/doubly-linked-list-c/doubly-linked-list/src/doubly-linked-list.c
@@ -26,3 +26,10 @@ void print_doubly_linked_list(struct doubly_linked_list *linked_list) {
         printf("TAIL\n");
     }
 }
+
+void print_doubly_linked_list_struct(struct doubly_linked_list *linked_list) {
+    print_doubly_linked_list(linked_list);
+    printf("\tpointer: %d\n", linked_list -> pointer);
+    printf("\tnumber_of_elements: %d\n", linked_list -> number_of_elements);
+    printf("\tused memory: %lu bytes\n", (linked_list -> number_of_elements * sizeof(struct doubly_linked_list_node)));
+}
