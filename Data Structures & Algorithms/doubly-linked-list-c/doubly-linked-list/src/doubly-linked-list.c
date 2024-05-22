ac47a60a7b16f3e5a395dd154b1c3a80972675a8
Sami Munir <sami.munir2001@gmail.com>
2024-05-22 08:40:53 -0400

    implementing function is_empty()

--- Data Structures & Algorithms/doubly-linked-list-c/doubly-linked-list/src/doubly-linked-list.c
+++ Data Structures & Algorithms/doubly-linked-list-c/doubly-linked-list/src/doubly-linked-list.c
@@ -2,3 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
+bool is_empty(struct doubly_linked_list *linked_list) {
+    if (linked_list -> head == NULL || linked_list -> pointer < 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
