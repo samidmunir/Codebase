780fadb0a7768a4d75a2b25d4a72343a024ad0d7
Sami Munir <sami.munir2001@gmail.com>
2024-05-22 08:37:35 -0400

    implementing doubly linked-list structure

--- Data Structures & Algorithms/doubly-linked-list-c/doubly-linked-list/src/doubly-linked-list.h
+++ Data Structures & Algorithms/doubly-linked-list-c/doubly-linked-list/src/doubly-linked-list.h
@@ -3,3 +3,10 @@ struct doubly_linked_list_node {
     struct doubly_linked_list_node *next;
     struct doubly_linked_list_node *prev;
 };
+
+struct doubly_linked_list {
+    struct doubly_linked_list_node *head;
+    struct doubly_linked_list_node *tail;
+    int pointer;
+    int number_of_elements;
+};
