00a9d59631d9734935d0567fa8f94e11d680c24f
Sami Munir <sami.munir2001@gmail.com>
2024-05-22 08:43:13 -0400

    implementing function print_doubly_linked_list()

--- Data Structures & Algorithms/doubly-linked-list-c/doubly-linked-list/src/doubly-linked-list.c
+++ Data Structures & Algorithms/doubly-linked-list-c/doubly-linked-list/src/doubly-linked-list.c
@@ -10,3 +10,19 @@ bool is_empty(struct doubly_linked_list *linked_list) {
         return false;
     }
 }
+
+void print_doubly_linked_list(struct doubly_linked_list *linked_list) {
+    printf("doubly-linked-list: ");
+    if (is_empty(linked_list)) {
+        printf("HEAD -> TAIL\n");
+        return;
+    } else {
+        struct doubly_linked_list_node *current_node = linked_list -> head;
+        printf("HEAD -> ");
+        while (current_node != NULL) {
+            printf("%d -> ", current_node -> data);
+            current_node = current_node -> next;
+        }
+        printf("TAIL\n");
+    }
+}
