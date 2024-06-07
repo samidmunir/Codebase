9eef6dadc620178fc5c54dbc20032fb4db37b6b9
Sami Munir <sami.munir2001@gmail.com>
2024-06-07 04:16:13 -0400

    working on Main component + styling

--- Projects/random-color-generator/src/App.js
+++ Projects/random-color-generator/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 
 export default function App() {
   return (
   <div id="App">
     <Header/>
+    <Main/>
   </div>
   );
 }
---
+++ Projects/random-color-generator/src/components/main/Main.jsx
@@ -0,0 +1,11 @@
+import "./Main.css";
+
+export default function Main() {
+    return (
+        <div id="Main">
+            <h1>Current color: </h1>
+            <div id="color-con"></div>
+            <button>Generate random color</button>
+        </div>
+    );
+}
