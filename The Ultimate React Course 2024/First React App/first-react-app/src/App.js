72153fbc84745517bdcd293aec0dd48968eb7b4f
Sami Munir <sami.munir2001@gmail.com>
2024-06-07 02:21:25 -0400

    creating a new component

--- The Ultimate React Course 2024/First React App/first-react-app/src/App.js
+++ The Ultimate React Course 2024/First React App/first-react-app/src/App.js
@@ -22,7 +22,13 @@ export default function App() {
     <div>
       <h1>{advice}</h1>
       <button onClick={getAdvice}>Get advice</button>
-      <p>You have read <strong>{count}</strong> pieces of advice.</p>
+      <Message count={count}/>
     </div>
+  );
+}
+
+function Message(props) {
+  return(
+    <p>You have read <strong>{props.count}</strong> pieces of advice.</p>
   );
 }
