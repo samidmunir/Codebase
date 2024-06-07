6685e36b33d767eca95dd3ea7caddf4cf14b5e19
Sami Munir <sami.munir2001@gmail.com>
2024-06-07 04:31:59 -0400

    implementing random color generation logic & UI complete

--- Projects/random-color-generator/src/components/main/Main.jsx
+++ Projects/random-color-generator/src/components/main/Main.jsx
@@ -1,11 +1,25 @@
+import {useEffect, useState} from "react";
 import "./Main.css";
 
 export default function Main() {
+
+    const [color, setColor] = useState("");
+
+    async function getColor() {
+        const res = await fetch("https://x-colors.yurace.pro/api/random");
+        const data = await res.json();
+        setColor(data.hex);
+    }
+
+    useEffect(function() {
+        getColor();
+    }, []);
+
     return (
         <div id="Main">
-            <h1>Current color: </h1>
-            <div id="color-con"></div>
-            <button>Generate random color</button>
+            <h1>{color}</h1>
+            <div id="color-con" style={{backgroundColor: color}}></div>
+            <button onClick={getColor}>Generate random color</button>
         </div>
     );
 }
