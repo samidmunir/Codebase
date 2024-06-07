fa55ef3d6986e05dbf148de33f9ef96e7f8f8f24
Sami Munir <sami.munir2001@gmail.com>
2024-06-07 02:13:08 -0400

    creating async fetch function & introducing useState() hook

--- The Ultimate React Course 2024/First React App/first-react-app/src/App.js
+++ The Ultimate React Course 2024/First React App/first-react-app/src/App.js
@@ -1,13 +1,21 @@
+import {useState} from "react";
+
 export default function App() {
+
+  const [advice, setAdvice] = useState("");
 
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    // console.log(data.slip.advice);
+    // alert(data.slip.advice);
+    setAdvice(data.slip.advice);
   }
 
   return(
     <div>
       <h1>Hello World!</h1>
-      <button>Get advice</button>
+      <button onClick={getAdvice}>Get advice</button>
     </div>
   );
 }
