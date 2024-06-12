92a4b4e7f0ba252b605b8eb9bc9e2673a45b722c
Sami Munir <sami.munir2001@gmail.com>
2024-06-12 03:00:10 -0400

    implementing Header component with more styling & placement

--- Projects/time-checker/src/components/header/Header.jsx
+++ Projects/time-checker/src/components/header/Header.jsx
@@ -1,10 +1,16 @@
 import "./Header.css";
+import { CiTimer } from "react-icons/ci";
 
 export default function Header() {
   return (
     <div className="Header">
       <div className="header-logo">
-        <h1>Time Checker</h1>
+        <h1>
+          Time Checker
+          <span>
+            <CiTimer />
+          </span>
+        </h1>
       </div>
       <div className="header-main">
         <h1>Breezy Dev Inc.</h1>
