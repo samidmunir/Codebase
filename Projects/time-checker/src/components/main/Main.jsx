c57fc7710f2cffa2df0ddc18fd9a700bdf0c9c85
Sami Munir <sami.munir2001@gmail.com>
2024-06-21 01:33:43 -0400

    starting new project: apple-clone

--- Projects/time-checker/src/components/main/Main.jsx
+++ Projects/time-checker/src/components/main/Main.jsx
@@ -1,15 +1,14 @@
 import "./Main.css";
-import hd_background_img from "../../assets/hd-background.webp";
+import hdBackgroundImg from "../../assets/hd-background.webp";
 
 export default function Main() {
   return (
     <div className="Main">
       <div className="main-section">
-        <h1>Current time</h1>
-        <div className="main-section-image">
-          <img src={hd_background_img} alt="high definition background" />
-        </div>
-        <p>Current location</p>
+        <h1>Eastern Standard Time</h1>
+      </div>
+      <div className="main-section-img-con">
+        <img src={hdBackgroundImg} alt="beautiful background" />
       </div>
     </div>
   );
