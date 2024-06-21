e1dfd3573e75a45a1675b2143a2e8de48c168254
Sami Munir <sami.munir2001@gmail.com>
2024-06-20 21:22:59 -0400

    starting LeetCode #3 - Java

---
+++ LeetCode Mastery/3. Longest Substring without Repeating Characters/longest-substring-wo-rept-chars/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\n3. Longest Substring without Repeating Characters");
+        /*
+         * Given a string s, find the length of the longest subtring without repeating
+         *  characters.
+         * 
+         * Example 1
+         *  input: s = "abcabcbb"
+         *  output: 3
+         * 
+         * Example 2
+         *  input: s = "bbbbb"
+         *  output: 1
+         * 
+         * Example 3
+         *  input: s = "pwwkew"
+         *  output: 3
+         */
+        String s1 = "abcabcbb";
+    }
+}
