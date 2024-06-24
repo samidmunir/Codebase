765613b01612962f71a084f4f80ff689ff645d5c
Sami Munir <sami.munir2001@gmail.com>
2024-06-23 22:32:12 -0400

    implementing JSX for App component

--- The Ultimate React Course 2024/Steps Flashcards/steps-flashcards/src/App.js
+++ The Ultimate React Course 2024/Steps Flashcards/steps-flashcards/src/App.js
@@ -1,3 +1,16 @@
 export default function App() {
-  return <div>Hello React</div>;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <p className="message">Hello</p>
+      <div className="buttons">
+        <button>Previous</button>
+        <button>Next</button>
+      </div>
+    </div>
+  );
 }
