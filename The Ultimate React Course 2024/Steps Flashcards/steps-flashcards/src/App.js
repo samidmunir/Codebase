9f2890b926f943b4e9653ff91cf9595a7179ae44
Sami Munir <sami.munir2001@gmail.com>
2024-06-23 22:41:56 -0400

    static JSX complete

--- The Ultimate React Course 2024/Steps Flashcards/steps-flashcards/src/App.js
+++ The Ultimate React Course 2024/Steps Flashcards/steps-flashcards/src/App.js
@@ -1,15 +1,29 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  const step = 1;
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
-      <p className="message">Hello</p>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
       <div className="buttons">
-        <button>Previous</button>
-        <button>Next</button>
+        <button style={{ backgroundColor: "#7950f2", color: "#ffffff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#ffffff" }}>
+          Next
+        </button>
       </div>
     </div>
   );
