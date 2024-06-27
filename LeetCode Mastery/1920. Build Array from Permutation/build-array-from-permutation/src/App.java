dc42e103bc7b64a4711d76aa31adaa38c0b77ab8
Sami Munir <sami.munir2001@gmail.com>
2024-06-26 23:29:02 -0400

    starting leetcode #1920

---
+++ LeetCode Mastery/1920. Build Array from Permutation/build-array-from-permutation/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\n1920. Build Array from Permutation");
+        /*
+         * Given a zero-based permutation nums (0-indexed), build an array ans of the same
+         *  length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+         * 
+         * A zero-based permutation nums is an array of distinct integers from 0 to
+         *  nums.length - 1 (inclusive).
+         * 
+         * Example 1
+         *  input: nums = [0, 2, 1, 5, 3, 4]
+         *  output: [0, 1, 2, 4, 5, 3]
+         */
+    }
+}
