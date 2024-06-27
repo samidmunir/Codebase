f7e0de17b594deb29fbbfee3611fcac014a7cb12
Sami Munir <sami.munir2001@gmail.com>
2024-06-27 03:43:59 -0400

    time-checker project complete + implementing dynamic time update using setInterval() function

--- Projects/time-checker/src/components/main/Main.jsx
+++ Projects/time-checker/src/components/main/Main.jsx
@@ -1,14 +1,23 @@
+import { useEffect, useState } from "react";
 import "./Main.css";
-import hdBackgroundImg from "../../assets/hd-background.webp";
 
 export default function Main() {
+  const [time, setTime] = useState(Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(Date()), 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <div className="Main">
       <div className="main-section">
-        <h1>Eastern Standard Time</h1>
-      </div>
-      <div className="main-section-img-con">
-        <img src={hdBackgroundImg} alt="beautiful background" />
+        <h1>{time.substring(25)}</h1>
+        <h2>{time.substring(0, 15)}</h2>
+        <div className="time-container">
+          <p>{time.substring(15, 25)}</p>
+        </div>
       </div>
     </div>
   );
