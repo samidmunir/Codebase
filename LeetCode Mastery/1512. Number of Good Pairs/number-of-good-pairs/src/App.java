de8e3ca5977bdf1139f77d7218f6f5d5c943da82
Sami Munir <sami.munir2001@gmail.com>
2024-06-27 20:38:39 -0400

    starting leetcode #1512 - java

---
+++ LeetCode Mastery/1512. Number of Good Pairs/number-of-good-pairs/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\n1512. Number of Good Pairs");
+        /*
+         * Given an array of integers nums, return the number of good pairs.
+         * 
+         * A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+         * 
+         * Example 1
+         *  input: nums = [1, 2, 3, 1, 1, 3]
+         *  output: 4
+         * 
+         * Example 2
+         *  input: nums = [1, 1, 1, 1]
+         *  output: 6
+         * 
+         * Example 3
+         *  input: nums = [1, 2, 3]
+         *  output: 0
+         */
+    }
+}
