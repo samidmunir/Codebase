fe8fc13df7d9d19ee531f442e62487909cfd880b
Sami Munir <sami.munir2001@gmail.com>
2024-07-01 03:28:49 -0400

    working on Navbar component

--- Projects/product-page/src/components/navbar/Navbar.jsx
+++ Projects/product-page/src/components/navbar/Navbar.jsx
@@ -1,17 +1,33 @@
 import React from "react";
+import { DiCodepen } from "react-icons/di";
+import "./Navbar.css";
 
 const navItems = [
-  { title: "Home", isActive: false },
-  { title: "About", isActive: false },
-  { title: "Products", isActive: true },
+  { id: 0, title: "Home", isActive: false },
+  { id: 1, title: "About", isActive: false },
+  { id: 2, title: "Products", isActive: true },
 ];
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div>
-        <h1>Breezy Solutions</h1>
+        <h1 id="navbar-title">
+          Breezy Solutions{" "}
+          <span id="navbar-title-logo">
+            <DiCodepen />
+          </span>
+        </h1>
       </div>
+      <ul id="navbar-items-list">
+        {navItems.map((item) => {
+          return (
+            <li key={item.id} className="navbar-items">
+              {item.title}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
