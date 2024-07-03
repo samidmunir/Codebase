fcc39943c69658094e319dfc5bbc345fa5f2de04
Sami Munir <sami.munir2001@gmail.com>
2024-07-03 03:40:55 -0400

    starting leetcode #605 (java)

---
+++ LeetCode Mastery/605. Can Place Flowers/can-place-flowers/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\n605. Can Place Flowers");
+        /*
+         * You have a long flowerbed in which some of the plots are planted, and some are not.
+         *  However, flowers cannot be planted in adjacent plots.
+         * 
+         * Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1
+         *  means not empty, and an integer n, return true if n flowers can be planted in the
+         *  flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+         * 
+         * Example 1
+         *  input: flowerbed = [1, 0, 0, 0, 1], n = 1
+         *  output: true
+         * 
+         * Example 2
+         *  input: flowerbed = [1, 0, 0, 0, 1], n = 2
+         *  output: false
+         */
+    }
+}
--- ReadMe.md
+++ ReadMe.md
@@ -2,6 +2,22 @@ Sami Munir | Rutgers University | sami.munir2001@outlook.com
 
 # Codebase
 
+## Leetcode Mastery
+
+### 1. Arrays
+
+#### 1. Two Sum
+
+#### 2. Add Two Numbers
+
+#### 3. Longest Substring without Repeating Characters
+
+#### 4. Median of Two Sorted Arrays
+
+#### 11. Container with Most Water
+
+#### 605. Can Place Flowers
+
 ## The Ultimate React Course 2024: React, Next.js, Redux & More
 
 ### Part 01 - Beginner
