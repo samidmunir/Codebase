ec860e940925bddc22d8278a44acca6bd339f138
Sami Munir <sami.munir2001@gmail.com>
2024-07-11 08:52:43 -0400

    implementing Navbar component-jsx

--- Projects/virtual-airline-manager/src/App.js
+++ Projects/virtual-airline-manager/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello React!</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 };
 
--- Projects/virtual-airline-manager/src/components/navbar/Navbar.jsx
+++ Projects/virtual-airline-manager/src/components/navbar/Navbar.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { FaThList } from "react-icons/fa";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="Navbar">
+      <div className="navbar-left">
+        <h1 className="navbar-left-title">
+          Virtual Airline Manager
+          <span className="navbar-left-logo">
+            <FaThList />
+          </span>
+        </h1>
+      </div>
+      <div className="navbar-center">
+        <ul className="navbar-center-ul">
+          <li>Home</li>
+          <li>About</li>
+          <li>Join Us</li>
+          <li>Agreement</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <div className="navbar-right-login">
+          <button className="navbar-login-btn1">Login</button>
+          <button className="navbar-login-btn2">Sign Up</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
