e802c7d437571e4fc8ee1ce2c4fbc4dea2a570d5
Sami Munir <sami.munir2001@gmail.com>
2024-07-21 23:41:34 -0500

    dynamic active-tab handling: useState()

--- Projects/virtual-airline-manager/src/components/navbar/Navbar.jsx
+++ Projects/virtual-airline-manager/src/components/navbar/Navbar.jsx
@@ -3,6 +3,12 @@ import { FaSitemap } from "react-icons/fa";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    // alert(`index: ${index}`);
+    setToggleState(index);
+  };
   return (
     <div className="Navbar">
       <div className="navbar-left">
@@ -15,19 +21,49 @@ const Navbar = () => {
       </div>
       <div className="navbar-center">
         <ul className="navbar-center-ul">
-          <li className="active-tab" id="nav-1">
+          <li
+            className={
+              toggleState === 1 ? "active-tab" : "navbar-center-ul-item"
+            }
+            id="nav-1"
+            onClick={() => toggleTab(1)}
+          >
             Home
           </li>
-          <li className="navbar-center-ul-item" id="nav-2">
+          <li
+            className={
+              toggleState === 2 ? "active-tab" : "navbar-center-ul-item"
+            }
+            id="nav-2"
+            onClick={() => toggleTab(2)}
+          >
             About
           </li>
-          <li className="navbar-center-ul-item" id="nav-3">
+          <li
+            className={
+              toggleState === 3 ? "active-tab" : "navbar-center-ul-item"
+            }
+            id="nav-3"
+            onClick={() => toggleTab(3)}
+          >
             Join Us
           </li>
-          <li className="navbar-center-ul-item" id="nav-4">
+          <li
+            className={
+              toggleState === 4 ? "active-tab" : "navbar-center-ul-item"
+            }
+            id="nav-4"
+            onClick={() => toggleTab(4)}
+          >
             Agreement
           </li>
-          <li className="navbar-center-ul-item" id="nav-5">
+          <li
+            className={
+              toggleState === 5 ? "active-tab" : "navbar-center-ul-item"
+            }
+            id="nav-5"
+            onClick={() => toggleTab(5)}
+          >
             Contact
           </li>
         </ul>
