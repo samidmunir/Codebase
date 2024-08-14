1f82e8efc4b272d393b83b3a2ab3e23e8c611967
Sami Munir <sami.munir2001@gmail.com>
2024-08-14 01:07:18 -0400

    implementing GlobalStyles.js

--- Projects/the-wild-oasis/src/App.jsx
+++ Projects/the-wild-oasis/src/App.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -31,12 +32,15 @@ const StyledApp = styled.div`
 
 const App = () => {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={() => alert("Check in")}>Check in</Button>
-      <Button onClick={() => alert("Check out")}>Check out</Button>
-      <Input type="number" placeholder="Number of guests" />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
   );
 };
 
