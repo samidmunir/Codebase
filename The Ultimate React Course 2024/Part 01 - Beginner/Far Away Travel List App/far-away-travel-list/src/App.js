6261fe5d54f94a2f88337f9ffb8b9d9bb3ab7843
Sami Munir <sami.munir2001@gmail.com>
2024-08-21 11:05:07 -0400

    building out the static layout by creating functional components

---
+++ The Ultimate React Course 2024/Part 01 - Beginner/Far Away Travel List App/far-away-travel-list/src/App.js
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items in your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
