15c9789d69fbb8d453d97fe51dc0ec2d3c0ed612
Sami Munir <sami.munir2001@gmail.com>
2024-09-02 16:42:54 -0400

    using prop drilling and child-to-parent communication to activate deleting feature for each item

--- The Ultimate React Course 2024/Part 01 - Beginner/Far Away Travel List App/far-away-travel-list/src/App.js
+++ The Ultimate React Course 2024/Part 01 - Beginner/Far Away Travel List App/far-away-travel-list/src/App.js
@@ -15,11 +15,15 @@ export default function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
   return (
     <>
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </>
   );
@@ -70,25 +74,25 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onDeleteItem={onDeleteItem} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
