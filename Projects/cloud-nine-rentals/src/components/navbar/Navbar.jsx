a5092a6ddc2504508334c1084ddd80331354f36d
Sami Munir <sami.munir2001@gmail.com>
2024-09-04 15:53:45 -0400

    adding icons to nav-list-items & creating data.js

--- Projects/cloud-nine-rentals/src/components/navbar/Navbar.jsx
+++ Projects/cloud-nine-rentals/src/components/navbar/Navbar.jsx
@@ -1,15 +1,89 @@
+import { useState } from "react";
 import "./Navbar.css";
 import NavUserCtls from "./NavUserCtls";
+import { MdDashboard } from "react-icons/md";
+import { FaServicestack } from "react-icons/fa";
+import { IoBookmarksSharp } from "react-icons/io5";
+import { MdManageAccounts } from "react-icons/md";
 
 function Navbar({ isSignedIn, setIsSignedIn }) {
+  const [activeListItem, setActiveListItem] = useState(1);
   return (
     <div className="Navbar">
       <h1 className="navbar-title dancing-script-bold">Cloud Nine Rentals</h1>
       <ul className="navbar-list">
-        <li className="abel-bold navbar-list-item">Dashboard</li>
-        <li className="abel-bold navbar-list-item">Services</li>
-        <li className="abel-bold navbar-list-item">Bookings</li>
-        <li className="abel-bold navbar-list-item">My Account</li>
+        <li
+          style={{ color: "#c5a880", fontSize: "18px", margin: "0px 25px" }}
+          className={
+            activeListItem === 1
+              ? "abel-bold active-list-item"
+              : "abel-bold navbar-list-item"
+          }
+          onClick={() => setActiveListItem(1)}
+        >
+          Dashboard{" "}
+          <MdDashboard
+            style={{
+              color: "#c5a880",
+              fontSize: "18px",
+              verticalAlign: "middle",
+            }}
+          />
+        </li>
+        <li
+          style={{ color: "#c5a880", fontSize: "18px", margin: "0px 25px" }}
+          className={
+            activeListItem === 2
+              ? "abel-bold active-list-item"
+              : "abel-bold navbar-list-item"
+          }
+          onClick={() => setActiveListItem(2)}
+        >
+          Services{" "}
+          <FaServicestack
+            style={{
+              color: "#c5a880",
+              fontSize: "18px",
+              verticalAlign: "middle",
+            }}
+          />
+        </li>
+        <li
+          style={{ color: "#c5a880", fontSize: "18px", margin: "0px 25px" }}
+          className={
+            activeListItem === 3
+              ? "abel-bold active-list-item"
+              : "abel-bold navbar-list-item"
+          }
+          onClick={() => setActiveListItem(3)}
+        >
+          Bookings{" "}
+          <IoBookmarksSharp
+            style={{
+              color: "#c5a880",
+              fontSize: "18px",
+              verticalAlign: "middle",
+            }}
+          />
+        </li>
+        <li
+          style={{ color: "#c5a880", fontSize: "18px", margin: "0px 25px" }}
+          className={
+            activeListItem === 4
+              ? "abel-bold active-list-item"
+              : "abel-bold navbar-list-item"
+          }
+          onClick={() => setActiveListItem(4)}
+        >
+          My Account{" "}
+          <MdManageAccounts
+            style={{
+              color: "#c5a880",
+              fontSize: "18px",
+              verticalAlign: "middle",
+            }}
+          />
+        </li>
       </ul>
       <NavUserCtls isSignedIn={isSignedIn} setIsSignedIn={setIsSignedIn} />
     </div>
---
+++ Projects/cloud-nine-rentals/src/data.js
@@ -0,0 +1,9 @@
+const user = {
+  firstName: "Rahameen",
+  lastName: "Khan",
+  email: "rahameenkhan@hotmail.com",
+  phoneNum: "+1 (908)-701-2023",
+  age: 22,
+  nationality: "United Kingdom",
+  validPassport: true,
+};
