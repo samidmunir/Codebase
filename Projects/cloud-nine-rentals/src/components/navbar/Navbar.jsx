b5cfe1a6fe5cc8e45dd89a81be833a9569c518cb
Sami Munir <sami.munir2001@gmail.com>
2024-09-04 11:57:12 -0400

    importing react-icons & working on static navbar component + navbar-user-con

--- Projects/cloud-nine-rentals/src/components/navbar/Navbar.jsx
+++ Projects/cloud-nine-rentals/src/components/navbar/Navbar.jsx
@@ -1,20 +1,32 @@
 import "./Navbar.css";
+import { FaUser } from "react-icons/fa";
+import { IoIosLogOut } from "react-icons/io";
 
 function Navbar() {
   return (
     <div className="Navbar">
-      <div className="navbar-title-con">
-        <h1 className="navbar-title dancing-script-bold">Cloud Nine Rentals</h1>
+      <h1 className="navbar-title dancing-script-bold">Cloud Nine Rentals</h1>
+      <ul className="navbar-list">
+        <li className="abel-bold navbar-list-item">Dashboard</li>
+        <li className="abel-bold navbar-list-item">Services</li>
+        <li className="abel-bold navbar-list-item">Bookings</li>
+        <li className="abel-bold navbar-list-item">My Account</li>
+      </ul>
+      <div className="navbar-user-con">
+        <FaUser
+          style={{ color: "#c5a880", fontSize: "24px", margin: "0px 25px" }}
+        />
+        <button className="abel-bold button navbar-user-logout-button">
+          Logout{" "}
+          <IoIosLogOut
+            style={{
+              color: "#c5a880",
+              verticalAlign: "middle",
+              fontSize: "20px",
+            }}
+          />
+        </button>
       </div>
-      <div className="navbar-list-con">
-        <ul className="navbar-list">
-          <li className="abel-regular navbar-list-item">Dashboard</li>
-          <li className="abel-regular navbar-list-item">Services</li>
-          <li className="abel-regular navbar-list-item">Bookings</li>
-          <li className="abel-regular navbar-list-item">My Account</li>
-        </ul>
-      </div>
-      <div className="navbar-user-con"></div>
     </div>
   );
 }
