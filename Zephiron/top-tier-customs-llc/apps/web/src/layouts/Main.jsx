5ead1016b9fdbe692d64e3784486fd29e4b3ed85
Sami Munir <samidmunir@outlook.com>
2025-12-23 15:48:38 -0500

    adding toast container to Main layout

--- Zephiron/top-tier-customs-llc/apps/web/src/layouts/Main.jsx
+++ Zephiron/top-tier-customs-llc/apps/web/src/layouts/Main.jsx
@@ -1,6 +1,7 @@
 import { useTheme } from "../contexts/Theme";
 import Navbar from "../components/Navbar";
 import ScrollBar from "../components/ui/ScrollBar";
+import { ToastContainer } from "react-toastify";
 
 const Main = ({ children }) => {
   const { theme } = useTheme();
@@ -10,6 +11,7 @@ const Main = ({ children }) => {
     <main>
       <ScrollBar />
       <Navbar />
+      <ToastContainer position="bottom-right" theme={theme} />
       <section
         className={`min-h-screen transition-all duration-3000 ${
           isDark ? "bg-zinc-950/90" : "bg-zinc-50/90"
