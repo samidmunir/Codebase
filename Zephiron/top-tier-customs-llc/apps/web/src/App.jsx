cc503ae0a53429ad0bd10632316eed16f1934bc9
Sami Munir <samidmunir@outlook.com>
2025-12-24 22:33:23 -0500

    implementing Auth page UI

--- Zephiron/top-tier-customs-llc/apps/web/src/App.jsx
+++ Zephiron/top-tier-customs-llc/apps/web/src/App.jsx
@@ -1,12 +1,14 @@
 import Main from "./layouts/Main";
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Auth from "./pages/Auth";
 
 const App = () => {
   return (
     <Main>
       <Routes>
         <Route path="/" index element={<Landing />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </Main>
   );
