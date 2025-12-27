65f6f7ac883ebbaf55cfc0ebfa9c060cde9384af
Sami Munir <samidmunir@outlook.com>
2025-12-26 22:47:11 -0500

    creating prelim. User model schema

---
+++ Zephiron/top-tier-customs/services/auth/src/models/User.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      required: true,
+    },
+    password_hash: {
+      type: String,
+      required: true,
+    },
+    roles: {
+      type: [String],
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+    profile: {
+      first_name: {
+        type: String,
+        required: true,
+      },
+      last_name: {
+        type: String,
+        required: true,
+      },
+    },
+    sec_ops: {
+      token_version: {
+        type: Number,
+      },
+      last_login: {
+        type: String,
+      },
+      failed_logins: {
+        type: Number,
+      },
+      last_password_reset: {
+        type: String,
+      },
+      failed_resets: {
+        type: Number,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("Users", userSchema);
+
+export default User;
