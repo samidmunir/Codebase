f9f8d9d6fe178393846f4123829b15bab6756eff
Sami Munir <samidmunir@outlook.com>
2026-01-13 15:41:38 +0000

    implementing EmbeddingManager class

--- Projects/rag-app/ingest.py
+++ Projects/rag-app/ingest.py
@@ -110,5 +110,45 @@ def split_documents(documents, chunk_size = 1000, chunk_overlap = 200):
 
     return split_docs
 
+"""
+Embedding & VectorStoreDB
+"""
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import chromadb
+from chromadb.config import Settings
+import uuid
+from typing import List, Dict, Any, Tuple
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+class EmbeddingManager:
+    def __init__(self, model_name: str = "all-MiniLM-L6-v2"):
+        self.model_name = model_name
+        self.model = None
+        self._load_model()
+
+    def _load_model(self):
+        try:
+            print(f"Loading embedding model: {self.model_name}")
+            self.model = SentenceTransformer(self.model_name)
+            print(f"Model loaded successfully. Embedding dimension: {self.model.get_sentence_embedding_dimension()}")
+        except Exception as e:
+            print(f"Error loading model {self.model_name}: {e}")
+            raise
+
+    def generate_embeddings(self, texts: List[str]) -> np.ndarray:
+        if not self.model:
+            raise ValueError("Model not loaded")
+        
+        print(f"Generating embeddings for {len(texts)} texts...")
+        embeddings = self.model.encode(texts, show_progress_bar = True)
+        print(f"Generated embeddings with shape: {embeddings.shape}")
+
+        return embeddings
+
 documents = process_pdfs("./data/pdfs")
 chunks = split_documents(documents = documents)
+
+# initialize the embedding manager
+embedding_manager = EmbeddingManager()
