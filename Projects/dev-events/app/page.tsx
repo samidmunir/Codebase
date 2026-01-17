6a4d9ee0204f0d9c86518d1197fc10f9443d0712
Sami Munir <samidmunir@outlook.com>
2026-01-17 18:45:22 -0500

    implementing LightRays component + setting up main Layout & Home page.tsx

--- Projects/dev-events/app/page.tsx
+++ Projects/dev-events/app/page.tsx
@@ -1,5 +1,14 @@
 const Page = () => {
-  return <h1>Welcome to Next.js 16!</h1>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+    </section>
+  );
 };
 
 export default Page;
