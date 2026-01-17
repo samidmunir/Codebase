660bc43128a67a41bbbfc2ebe8109fd7aea0c469
Sami Munir <samidmunir@outlook.com>
2026-01-17 18:59:22 -0500

    implementing Navbar component & working on Home page UI

--- Projects/dev-events/app/page.tsx
+++ Projects/dev-events/app/page.tsx
@@ -1,3 +1,5 @@
+import ExploreBtn from "./components/ExploreBtn";
+
 const Page = () => {
   return (
     <section>
@@ -7,6 +9,15 @@ const Page = () => {
       <p className="text-center mt-5">
         Hackathons, Meetups, and Conferences, All in One Place
       </p>
+      <ExploreBtn />
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events">
+          {[1, 2, 3, 4, 5].map((event) => (
+            <li key={event}>Event {event}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
