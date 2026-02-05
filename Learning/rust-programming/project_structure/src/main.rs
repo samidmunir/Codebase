8c17b6bf900c2067b6103190fe91a818e59c810e
Sami Munir <samidmunir@outlook.com>
2026-02-04 19:49:44 -0500

    learning Rust project structure

---
+++ Learning/rust-programming/project_structure/src/main.rs
@@ -0,0 +1,22 @@
+/*
+    Our previous Rust programs all lived in a single main.rs file.
+
+    When we run the cargo new command, we create a new Rust package.
+    - a package is a folder with a Cargo.toml file.
+        > the Cargo.toml file holds metadata about the package like its name and version.
+
+    A package is a collection of one or more crates.
+    - a crate is a collection of Rust code that produces an executable or a library.
+    - a crate is the smallest amount of code that the Rust compiler considers at a time.
+
+    Types of Crates
+    - Binary Crate
+        > a crate that compiles to an executable.
+            * a binary crate has a main function that is the entrypoint for the executable.
+    - Library Cate
+        > exports functionality for other Rust programs to share and use.
+            * a library crate does not have a main function and does not compile to be an executable program.
+*/
+
+fn main() {
+}
