08bf8239e4e3c0d5db5cea5bc011355bf89fd9f5
Sami Munir <samidmunir@outlook.com>
2026-02-11 22:34:41 -0500

    learning Strings in Rust

---
+++ Learning/rust-programming/strings/src/main.rs
@@ -0,0 +1,15 @@
+/*
+    Strings in Rust
+
+    A String is a piece of text or, alternatively, a sequence of characters.
+
+    &str -> String slice (reference/borrowed/immutable)
+
+    String -> lives on Heap (mutable/ownership)
+    - let empty: String = String::new();
+    - let temp: String = String::from("Rust!");
+*/
+
+fn main() {
+    println!("\nStrings in Rust!\n");
+}
