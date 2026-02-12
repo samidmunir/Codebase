9a8923c235dc2f1666063b123806dc84d8a2e459
Sami Munir <samidmunir@outlook.com>
2026-02-11 22:49:24 -0500

    completing testng setUp() method

---
+++ Learning/java-programming/src/test/java/selenium_part_one/FirstTest.java
@@ -0,0 +1,16 @@
+package selenium_part_one;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
+public class FirstTest {
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+}
