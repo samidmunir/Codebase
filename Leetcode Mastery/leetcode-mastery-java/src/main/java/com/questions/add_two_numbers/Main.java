e26ad6d3abdeda7bfe2b531ea23d680f9a627ee2
Sami Munir <samidmunir@outlook.com>
2026-02-19 15:22:00 -0500

    adding problem #2 Add Two Numbers (java solution)

---
+++ Leetcode Mastery/leetcode-mastery-java/src/main/java/com/questions/add_two_numbers/Main.java
@@ -0,0 +1,11 @@
+package com.questions.add_two_numbers;
+
+public class Main {
+    /*
+        You are given two non-empty linked lists representing two non-negative integers. The digits are stores in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+        You may assume the two numbers do not contain any leading zeros, except the number 0 itself.
+    */
+    public static void main(String[] args) {
+    }
+}
---
+++ Leetcode Mastery/leetcode-mastery-java/src/main/java/com/questions/add_two_numbers/SolutionOne.java
@@ -0,0 +1,6 @@
+package com.questions.add_two_numbers;
+
+public class SolutionOne {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    }
+}
