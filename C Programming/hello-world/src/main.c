03bbc5b09bcfbfa6ed99b40171aa23212b0519de
Sami Munir <samidmunir@outlook.com>
2026-04-01 17:53:31 -0400

    adding comments/notes to hello-world

--- C Programming/hello-world/src/main.c
+++ C Programming/hello-world/src/main.c
@@ -1,6 +1,12 @@
+/*
+    Pre-processor include directive.
+*/
 #include <stdio.h>
 #include "hello.h"
 
+/*
+    Main function -> entry point of the program.
+*/
 int main(void) {
     print_hello();
     
