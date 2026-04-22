a33b74f4a38c3f10d10194d1a22052eb885e6051
Sami Munir <samidmunir@outlook.com>
2026-04-22 18:53:22 -0400

    learning C++ program structure: namespaces, console IO (input/output)

--- C++ Programming/hello-world/src/main.cpp
+++ C++ Programming/hello-world/src/main.cpp
@@ -15,6 +15,15 @@
 #include <iostream>
 
 /*
+    Namespaces are containers to group code entities into namespace scopes.
+    - reduces naming conflicts
+    - std is the name for the C++ "standard" namespace
+    - utilize the scope resolution operator "::"
+*/
+
+using namespace std;
+
+/*
     Every C++ program must have exactly 1 main() function.
     - starting point of program execution
     - return 0 indicates successful program execution
@@ -22,9 +31,36 @@
     - argv (**) -> pointer to the vector of program arguments
 */
 int main(int argc, char *argv[]) {
-    std::cout << "\nHello world!" << std::endl;
-    std::cout << "-> Welcome to C++ Programming!" << std::endl;
-    std::cout << "------------------------------\n" << std::endl;
+    /*
+        cout, cin, cerr, clog are objects representing streams.
 
+        cout - standard output stream
+            > console/terminal
+            > console output
+        cin - standard input stream
+            > keyboard
+            > console input
+        << - insertion operator
+            > output streams
+        >> - extraction operator
+            > input streams
+
+        cout and <<
+        - insert the data into the cout stream
+        - can be chained
+        - does not automatically add line breaks
+
+        cin and >>
+        - extracts data from the cin stream based on data type
+        - can be chained
+        - can fail if the entered data cannot be interpreted
+    */
+    cout << "\nHello world!" << endl;
+    cout << "-> Welcome to C++ Programming!" << endl;
+    cout << "------------------------------\n" << endl;
+
+    /*
+        Same as return 0;
+    */
     return EXIT_SUCCESS;
 }
