bc796e06739ac3b3d10844b431f135289d357042
Sami Munir <samidmunir@outlook.com>
2026-05-06 17:07:54 -0400

    defining functions with arguments

--- Ruby Programming/hello-ruby/main.rb
+++ Ruby Programming/hello-ruby/main.rb
@@ -10,3 +10,10 @@
 puts "Hello, welcome to Ruby programming!" # has a newline
 p "Hello, welcome to Ruby programming!"
 print "Hello, welcome to Ruby programming!" # does not have a newline
+
+# Defining a function say_hello to print out a message which is passed in as an argument
+def say_hello(message)
+  puts message
+end
+
+say_hello "\nHello, welcome to Ruby programming!"
