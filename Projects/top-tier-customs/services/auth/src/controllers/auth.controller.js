76ade898e19a19d338822eafa28a63109d0f745e
Sami Munir <samidmunir@outlook.com>
2026-05-08 19:00:56 -0400

    implementing API.auth.signup() controller & route

--- Projects/top-tier-customs/services/auth/src/controllers/auth.controller.js
+++ Projects/top-tier-customs/services/auth/src/controllers/auth.controller.js
@@ -1,4 +1,7 @@
 import ENV from "../config/env.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
 
 export const health = async (_req, res) => {
   return res.status(200).json({
@@ -6,4 +9,73 @@ export const health = async (_req, res) => {
     source: "<api.auth>: health()",
     message: `/api/auth is live on http://localhost:${ENV.PORT}`,
   });
+};
+
+export const signup = async (req, res) => {
+  try {
+    const { email, password, firstName, lastName, city, country, phone } =
+      req.body;
+
+    if (
+      !email ||
+      !password ||
+      !firstName ||
+      !lastName ||
+      !city ||
+      !country ||
+      !phone
+    ) {
+      return res.status(400).json({
+        ok: false,
+        source: "<api.auth.controller>: signup()",
+        message: "Failed to signup",
+        error: "Missing required fields.",
+      });
+    }
+
+    const dbUser = await User.findOne({ email });
+    if (dbUser) {
+      return res.status(400).json({
+        ok: false,
+        source: "<api.auth.controller>: signup()",
+        message: "Failed to signup.",
+        error: "Email already in use.",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      email: email,
+      passwordHash: hashedPassword,
+      profile: {
+        firstName: firstName,
+        lastName: lastName,
+        phone: phone,
+        city: city,
+        country: country,
+      },
+    });
+
+    const userRes = {
+      _id: user._id,
+      email: user.email,
+      role: user.role,
+      profile: user.profile,
+    };
+
+    return res.status(201).json({
+      ok: true,
+      source: "<api.auth.controller>: signup()",
+      message: "Signup successful.",
+      user: userRes,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      ok: false,
+      source: "<api.auth.controller>: signup()",
+      message: "Failed to signup.",
+      error: "Internal server error.",
+    });
+  }
 };
