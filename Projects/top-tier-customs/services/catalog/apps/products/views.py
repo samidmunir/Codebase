a1a243326038791d03728bfe0fc99831d3b212da
Sami Munir <samidmunir@outlook.com>
2026-07-06 10:44:47 -0400

    implementing Product ModelViewSet

--- Projects/top-tier-customs/services/catalog/apps/products/views.py
+++ Projects/top-tier-customs/services/catalog/apps/products/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from models import Product
+from serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filterset_fields = ['category', 'brand', 'is_active', 'is_featured']
+    search_fields = ['name', 'description', 'sku', 'category', 'brand']
+    ordering_fields = ['price', 'created_at', 'updated_at', 'stock_quantity']
