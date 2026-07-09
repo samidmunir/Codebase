9a1e324bf46931d9d31c2575caa6363957ff885b
Sami Munir <samidmunir@outlook.com>
2026-07-09 08:32:56 -0400

    refactoring Catalog > Service routes to handle admin/public routing

--- Projects/top-tier-customs/services/catalog/apps/services/urls.py
+++ Projects/top-tier-customs/services/catalog/apps/services/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import ServiceViewSet
+from .views import ServicePublicViewSet, ServiceAdminViewSet
 
 router = DefaultRouter()
-router.register(r'services', ServiceViewSet, basename = 'service')
+router.register(r"services", ServicePublicViewSet, basename="public-service")
+router.register(r"admin/services", ServiceAdminViewSet, basename="admin-service")
 
 urlpatterns = router.urls
