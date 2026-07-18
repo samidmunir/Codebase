57ad8e1732d13820050ad575c3e404687016cefc
Sami Munir <samidmunir@outlook.com>
2026-07-18 15:37:03 -0400

    refactoring Vite React frontend, implementing Home Page with API/database health check UI/UX

--- Projects/go-job-tracker/client/src/main.jsx
+++ Projects/go-job-tracker/client/src/main.jsx
@@ -1,3 +1,4 @@
+/*
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -8,3 +9,18 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+*/
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+);
