8e6f421dd1388ca7b976a2f92735eeb653d4f01e
Sami Munir <samidmunir@outlook.com>
2026-07-17 23:18:35 -0400

    implementing handler func Check() to verify API health

--- Projects/go-job-tracker/server/internal/health/handler.go
+++ Projects/go-job-tracker/server/internal/health/handler.go
@@ -1,9 +1,13 @@
 package health
 
 import (
+	"net/http"
+	"runtime"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/samidmunir/go-job-tracker/server/internal/database"
+	"github.com/samidmunir/go-job-tracker/server/internal/shared/response"
 )
 
 type Handler struct {
@@ -20,4 +24,20 @@ func NewHandler(mongoDB *database.MongoDB, appName string, environment string) *
 		appName: appName,
 		env: environment,
 	};
+}
+
+func (h *Handler) Check(ctx *gin.Context) {
+	response.JSON(
+		ctx,
+		http.StatusOK,
+		"API is healthy",
+		gin.H{
+			"service": h.appName,
+			"environment": h.env,
+			"status": "healthy",
+			"timestamp": time.Now().UTC(),
+			"uptime": time.Since(h.startedAt).String(),
+			"goVersion": runtime.Version(),
+		},
+	);
 }
