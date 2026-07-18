fe686d3c5480fd9c5bdb9d2ef99b816b0b15410c
Sami Munir <samidmunir@outlook.com>
2026-07-18 16:51:03 -0400

    implementing password utility for auth - func ValidatePassword()

--- Projects/go-job-tracker/server/internal/auth/password.go
+++ Projects/go-job-tracker/server/internal/auth/password.go
@@ -1,6 +1,9 @@
 package auth
 
 import (
+	"errors"
+	"unicode"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -25,4 +28,34 @@ func VerifyPassword(password string, passwordHash string) bool {
 	);
 
 	return err == nil;
+}
+
+func ValidatePassword(password string) error {
+	if len(password) < 10 {
+		return  errors.New("password must contain at least 10 characters");
+	}
+
+	var hasUpper bool;
+	var hasLower bool;
+	var hasNumber bool;
+	var hasSymbol bool;
+	
+	for _, character := range password {
+		switch {
+		case unicode.IsUpper(character):
+			hasUpper = true;
+		case unicode.IsLower(character):
+			hasLower = true;
+		case unicode.IsNumber(character):
+			hasNumber = true;
+		case unicode.IsPunct(character) || unicode.IsSymbol(character):
+			hasSymbol = true;
+		}
+	}
+
+	if !hasUpper || !hasLower || !hasNumber || !hasSymbol {
+		return errors.New("password must contain uppercase, lowercase, number, and special characters");
+	}
+
+	return nil;
 }
