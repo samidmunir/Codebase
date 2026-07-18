86f57abb9df25dcb4f7625ade1c2c6fab2b053f6
Sami Munir <samidmunir@outlook.com>
2026-07-17 23:12:26 -0400

    implementing recovery middleware

---
+++ Projects/go-job-tracker/server/internal/middleware/recovery.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"log/slog"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Recovery() gin.HandlerFunc {
+	return gin.CustomRecovery(func(ctx *gin.Context, recovered interface{}) {
+		slog.Error(
+			"panic recovered",
+			"error",
+			recovered,
+			"path",
+			ctx.Request.URL.Path,
+		);
+	});
+}
