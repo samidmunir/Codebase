6d7cb4d0c3fac13f07eb353f478c092592c103fd
Sami Munir <samidmunir@outlook.com>
2026-07-20 17:39:28 -0400

    implementing token store

---
+++ Projects/go-job-tracker/client/src/api/tokenStore.js
@@ -0,0 +1,13 @@
+let accessToken = null;
+
+export function getAccessToken() {
+  return accessToken;
+}
+
+export function setAccessToken(token) {
+  accessToken = token || null;
+}
+
+export function clearAccessToken() {
+  accessToken = null;
+}
